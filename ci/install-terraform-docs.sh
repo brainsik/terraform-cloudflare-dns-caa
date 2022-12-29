fb7f168a48010cb6bbab02a986cbcea81888c678
jeremy avnet <brainsik@users.noreply.github.com>
2022-12-29 01:07:10 -0800

    Improve install scripts output

--- ci/install-terraform-docs.sh
+++ ci/install-terraform-docs.sh
@@ -5,23 +5,23 @@ tfdocs_bin="$HOME/bin/terraform-docs-$TERRAFORM_DOCS_VER"
 
 # Bail out if we already have this version installed
 if [[ -x "$tfdocs_bin" ]]; then
-    echo "Found $tfdocs_bin"
+    echo "✨ Found $tfdocs_bin"
     exit 0
 fi
 
-# Create ~/bin if it doesn't exist
+# Ensure ~/bin exists
 mkdir -p "$HOME/bin"
 
 # Grab the zip file from GitHub releases
-pushd "$(mktemp -d -t terraform-docs.XXXXXXXX)"
-curl --fail -sS -L -o terraform-docs.tar.gz "https://github.com/terraform-docs/terraform-docs/releases/download/$TERRAFORM_DOCS_VER/terraform-docs-$TERRAFORM_DOCS_VER-linux-amd64.tar.gz"
+pushd "$(mktemp -d -t terraform-docs.XXXXXXXX)" >/dev/null
+curl --fail -L -o terraform-docs.tar.gz "https://github.com/terraform-docs/terraform-docs/releases/download/$TERRAFORM_DOCS_VER/terraform-docs-$TERRAFORM_DOCS_VER-linux-amd64.tar.gz"
 tar -xzf terraform-docs.tar.gz
 chmod +x terraform-docs
 mv terraform-docs "$tfdocs_bin"
-popd
+popd >/dev/null
 
 # Move binary to ~/bin and symlink
-pushd "$HOME/bin"
+pushd "$HOME/bin" >/dev/null
 rm -f terraform-docs
 ln -s "$tfdocs_bin" terraform-docs
-popd
+popd >/dev/null
--- ci/install-tflint.sh
+++ ci/install-tflint.sh
@@ -5,7 +5,7 @@ tflint_bin="$HOME/bin/tflint-$TFLINT_VER"
 
 # Bail out if we already have this version installed
 if [[ -x "$tflint_bin" ]]; then
-    echo "Found $tflint_bin"
+    echo "✨ Found $tflint_bin"
     exit 0
 fi
 
@@ -13,15 +13,15 @@ fi
 mkdir -p "$HOME/bin"
 
 # Grab the zip file from GitHub releases
-pushd "$(mktemp -d -t tflint.XXXXXXXX)"
-curl --fail -sS -L -o tflint.zip "https://github.com/terraform-linters/tflint/releases/download/${TFLINT_VER}/tflint_linux_amd64.zip"
+pushd "$(mktemp -d -t tflint.XXXXXXXX)" >/dev/null
+curl --fail -L -o tflint.zip "https://github.com/terraform-linters/tflint/releases/download/${TFLINT_VER}/tflint_linux_amd64.zip"
 unzip -u tflint.zip
 chmod +x tflint
 mv tflint "$tflint_bin"
-popd
+popd >/dev/null
 
 # Move binary to ~/bin and symlink
-pushd "$HOME/bin"
+pushd "$HOME/bin" >/dev/null
 rm -f tflint
 ln -s "$tflint_bin" tflint
-popd
+popd >/dev/null
